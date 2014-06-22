382f2ebd1e3640b586dcfb498ba96bfba4a9f7aa
Iñaki Reta Sabarrós <ignacio.reta@gmail.com>
2014-06-22 23:05:32 +0200

    Implementado el budge de checkstyle

--- checkstyle.js
+++ checkstyle.js
@@ -1,8 +1,38 @@
-var generateData = function(data){
-  data.subject= 'checkstyle';
-  data.status= 'ok';
-  data.color= 'BF00FF';
-  return data;
+var fs = require('fs');
+var cheerio = require('cheerio');
+
+var calculateColor = function(status){
+  if(status === 0 ){
+    return 'brightgreen';
+  }else if (status > 0 && status <= 2000){
+    return 'green';
+  }else if (status > 2000 && status <= 4000){
+    return 'yellowgreen';
+  }else if (status > 4000 && status <= 6000){
+    return 'yellow';
+  }else if (status > 6000 && status <= 8000){
+    return 'orange';
+  }else if (status > 8000 && status <= 10000){
+    return 'red';
+  }else {
+    return 'FF0000';
+  }
 }
 
-module.exports.analysis = generateData;
+
+var analyze = function(budge, shields){
+  fs.readFile(budge.path+budge.file+'.xml', function(err, data){
+    var $ = cheerio.load(data.toString(), {xmlMode: true});
+    budge.status = $('error').toArray().length+ ' errors';
+    budge.color = calculateColor(budge.status);
+    shields(budge);
+  });
+}
+
+module.exports.analisys = function(data, shields){
+  data.file = 'checkstyle-result'
+  data.subject= 'checkstyle';
+  data.status= 'fail';
+  data.color= 'BF00FF';
+  analyze(data, shields);
+};
