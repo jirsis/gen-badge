6c7971390f6757f10ae594675105a84d86d1d3fb
Iñaki Reta Sabarrós <ignacio.reta@gmail.com>
2014-07-12 16:15:19 +0200

    Separados en distintos niveles de criticidad para checktyle

--- checkstyle.js
+++ checkstyle.js
@@ -1,28 +1,89 @@
 var fs = require('fs');
 var cheerio = require('cheerio');
 
-var calculateColor = function(status){
-  if(status === 0 ){
-    return 'brightgreen';
-  }else if (status > 0 && status <= 2000){
-    return 'green';
-  }else if (status > 2000 && status <= 4000){
-    return 'yellowgreen';
-  }else if (status > 4000 && status <= 6000){
-    return 'yellow';
-  }else if (status > 6000 && status <= 8000){
-    return 'orange';
+var colors = ['brightgreen', 'green', 'yellowgreen', 'yellow', 'orange', 'red'];
+
+var calculateColorInfos = function(infos){
+  var color;
+  if(infos === 0 ){
+    color = 0;
+  }else if (infos > 0 && infos <= 2000){
+    color = 1;
+  }else if (infos > 2000 && infos <= 4000){
+    color = 2;
+  }else if (infos > 4000 && infos <= 6000){
+    color = 3;
+  }else if (infos > 6000 && infos <= 8000){
+    color = 4;
   }else {
-    return 'red';
+    color = 5;
   }
+  return color;
+}
+
+var calculateColorWarnings = function(warnings, color){
+  var warningColor;
+  if(warnings === 0){
+    warningColor = 0;
+  }else if(warnings > 0 && warnings <= 25){
+    warningColor = 1;
+  }else if(warnings > 25 && warnings <= 75){
+    warningColor = 2;
+  }else if (warnings > 75 && warnings <= 125){
+    warningColor = 3;
+  }else if (warnings > 125 && warnings <= 200){
+    warningColor = 4;
+  }else{
+    warningColor = 5;
+  }
+  return Math.max(color, warningColor);
+}
+
+var calculateColorErrors = function(errors, color){
+  var errorColor;
+  if(errors === 0){
+    errorColor = 0;
+  }else if(errors > 0 && errors <= 5){
+    errorColor = 1;
+  }else if(errors > 5 && errors <= 10){
+    errorColor = 2;
+  }else if (errors > 10 && errors <= 15){
+    errorColor = 3;
+  }else if (errors > 15 && errors <= 20){
+    errorColor = 4;
+  }else{
+    errorColor = 5;
+  }
+
+  return Math.max(color, errorColor);
+}
+
+var calculateColor = function(info, warning, error){
+  var color = calculateColorInfos(info);
+  color = calculateColorWarnings(warning, color);
+  color = calculateColorErrors(error, color);
+  return colors[color];
 }
 
 
 var analyze = function(budge, shields){
   fs.readFile(budge.path+budge.file+'.xml', function(err, data){
     var $ = cheerio.load(data.toString(), {xmlMode: true});
+    var infos = 0;
+    var warnings = 0;
+    var errors = 0;
     budge.status = $('error').toArray().length+ ' errors';
-    budge.color = calculateColor(budge.status);
+    $('error').toArray().forEach(function(item, id){
+      if($(item).attr('severity') === 'warning'){
+        warnings++;
+      }else if($(item).attr('severity') === 'info'){
+        infos++;
+      }else {
+        errors++;
+      }
+    });
+    budge.status = 'E:'+errors + ' W:' + warnings + ' I:'+infos;
+    budge.color = calculateColor(infos, warnings, errors);
     shields(budge);
   });
 }
